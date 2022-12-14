eaa651788221a2f0278f77558148104b821cb77f
Макаров Игорь (Makarov_IV) <Makarov_IV@directum.ru>
2022-12-15 02:57:24 +0400

    feat: добавил создание проекта

--- RXInstanceManager/MainWindow.xaml.cs
+++ RXInstanceManager/MainWindow.xaml.cs
@@ -349,7 +349,6 @@ namespace RXInstanceManager
         if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
         {
           var config_filename = openFileDialog.FileName;
-
           try
           {
               AppHandlers.LaunchProcess(AppHelper.GetDoPath(_instance.InstancePath), string.Format("map set {0} -rundds=False -need_pause", config_filename), true, true);
@@ -363,6 +362,32 @@ namespace RXInstanceManager
       }
     }
 
+    private void CreateProject_Click(object sender, RoutedEventArgs e)
+    {
+      AppHandlers.InfoHandler(_instance, MethodBase.GetCurrentMethod().Name);
+      using (System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog())
+      {
+        var filter = string.Format("configs for {0}|{0}_*.yml;{0}_*.yaml|YAML-файлы|*.yml;*.yaml|All files (*.*)|*.*", _instance.Code);
+        openFileDialog.InitialDirectory = string.IsNullOrEmpty(_instance.ProjectConfigPath) ? "C:\\" : Path.GetDirectoryName(_instance.ProjectConfigPath);
+        openFileDialog.Filter = filter;
+        openFileDialog.FilterIndex = 1;
+        openFileDialog.RestoreDirectory = true;
+
+        if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+        {
+          var config_filename = openFileDialog.FileName;
+          try
+          {            
+            AppHandlers.LaunchProcess(AppHelper.GetDoPath(_instance.InstancePath), string.Format("map create_project {0} -rundds=False -need_pause", config_filename), true, true);
+          }
+          catch (Exception ex)
+          {
+            AppHandlers.ErrorHandler(_instance, ex);
+          }
+
+        }
+      }
+    }
     private void UpdateConfig_Click(object sender, RoutedEventArgs e)
     {
       AppHandlers.InfoHandler(_instance, MethodBase.GetCurrentMethod().Name);
@@ -379,6 +404,7 @@ namespace RXInstanceManager
           var config_filename = openFileDialog.FileName;
           try
           {
+            
               AppHandlers.LaunchProcess(AppHelper.GetDoPath(_instance.InstancePath), string.Format("map update_config {0} -rundds=False -need_pause", config_filename), true, true);
           }
           catch (Exception ex)
